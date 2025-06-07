da0cedee7604c299600993ef0485fbf425914685
pGUPT4 <pratbro1@gmail.com>
2025-06-07 10:30:48 +0530

    commit

--- frontend/app/hooks/index.tsx
+++ frontend/app/hooks/index.tsx
@@ -1,3 +1,8 @@
 export {default as useLogin} from './use-login';
 export {default as useSignUp} from './use-signup';
 export {default as useHome} from './use-logout'
+export {default as useLogout} from './use-logout';
+
+export {default as useChatAll} from './use-chat-all';
+export {default as useChatCreate} from './use-chat-create';
+export {default as useChatDelete} from './use-chat-delete';
---
+++ frontend/app/hooks/use-chat-all.tsx
@@ -0,0 +1,49 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { useCreateChatMutation } from '../../redux/features/chatApiSlice';
+import { useLoginMutation } from '../../redux/features/authApiSlice';
+import { useDispatch } from 'react-redux';
+import { setAuth } from '../../redux/features/authSlice';
+
+export default function useChatAll() {
+    const dispatch = useDispatch();
+	const router = useRouter();
+
+    const [login, {isLoading}] = useLoginMutation();
+
+	const [inputValue, setInputValue] = useState({
+		email: '',
+		password: '',
+	});
+
+	const { email, password } = inputValue;
+
+	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+
+		setInputValue({ ...inputValue, [name]: value });
+	};
+
+	const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+        login({ email, password })
+            .unwrap()
+            .then(() => {
+                dispatch(setAuth());
+                toast.success('Logged in');
+                router.push('/');
+            })
+            .catch(() => {
+                toast.error('Failed to log in');
+            });
+	};
+
+	return {
+		email,
+		password,
+		onChange,
+		onSubmit,
+	};
+}
